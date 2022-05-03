2a6e5eb8d2ddd2ba9471fbc6d26f122af471751d
Md. Amir Hossain Jowel <mdamirhossainjowel@gmail.com>
2022-05-04 00:22:06 +0600

    post method done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient } = require("mongodb");
 
 const app = express();
 app.use(cors());
@@ -14,15 +14,32 @@ app.listen(port, () => {
   console.log(port);
 });
 
+// Replace the uri string with your MongoDB deployment's connection string.
 const uri =
-  "mongodb+srv://dbAlaeze:<Alaeze>@cluster0.lhcdr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  console.log("dB Connected");
-  client.close();
-});
+  "mongodb+srv://dbAlaeze:Alaeze@cluster0.lhcdr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("ktsofficial");
+    const Products = database.collection("products");
+
+    app.post("/product", async (req, res) => {
+      const newProduct = req.body;
+
+      const result = await Products.insertOne(newProduct);
+      res.send(result);
+    });
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = Products.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
