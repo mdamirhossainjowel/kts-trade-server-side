ea726da536ce5f33b0405849322d2ab545512aee
Md. Amir Hossain Jowel <mdamirhossainjowel@gmail.com>
2022-05-03 14:49:01 +0600

    updated

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello There");
+});
+app.listen(port, () => {
+  console.log(port);
+});
+
+const uri =
+  "mongodb+srv://dbAlaeze:<Alaeze>@cluster0.lhcdr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  console.log("dB Connected");
+  client.close();
+});
